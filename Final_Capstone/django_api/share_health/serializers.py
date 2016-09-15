eff0472b0ddacd048a3fe3885b72002b37a11ad4
Keith <keithbradley1@msn.com>
2016-09-15 17:14:12 -0500

    made serializers

--- Final_Capstone/django_api/share_health/serializers.py
+++ Final_Capstone/django_api/share_health/serializers.py
@@ -4,14 +4,14 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'url', 'name')
+        fields = ('id', 'url', 'name', 'exercise', 'nutrition')
 
 class NutritionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Nutrition
-        fields = ('id', 'url', 'name', 'nutrition')
+        fields = ('id', 'url', 'name', 'nutrition', 'description', 'amount', 'unit', 'calories', 'carbs', 'lipids')
 
 class ExerciseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Exercise
-        fields = ('id', 'url', 'name', 'exercise')
+        fields = ('id', 'url', 'name', 'exercise', 'description', 'amount', 'unit', 'calories')
