1375fd253351d8d15a113d88fdc6bdb1feb3d9aa
Keith <keithbradley1@msn.com>
2016-09-16 11:24:01 -0500

    uncomment landingConfig

--- Final_Capstone/angular/app/landing/landingConfig.js
+++ Final_Capstone/angular/app/landing/landingConfig.js
@@ -1,8 +1,8 @@
-// angular.module('Share_Health')
-//   .config(($routeProvider) => {
-//     $routeProvider
-//       .when("/", {
-//         controller: "LandingCtrl",
-//         templateUrl: "/app/landing/landing.html"
-//       })
-//   })
+angular.module('Share_Health')
+  .config(($routeProvider) => {
+    $routeProvider
+      .when("/", {
+        controller: "LandingCtrl",
+        templateUrl: "/app/landing/landing.html"
+      })
+  })
