66762e42ceabed1380b01ff1f47c317aa3a9518c
Keith <keithbradley1@msn.com>
2016-09-16 11:17:46 -0500

    call the rootAPI and create a capitalize filter in app.js

---
+++ Final_Capstone/angular/app/app.js
@@ -0,0 +1,17 @@
+angular.module('Share_Health', ['ngRoute'])
+  .factory('RootFactory', ($http, $timeout) => {
+    let apiRoot = null;
+    let httpGet = $http.get('http://localhost:8000');
+
+    return {
+      getApiRoot: () => {
+          return httpGet.then(res => res.data)
+      }
+    }
+  }
+)
+.filter('capitalize', () => {
+  return (thingToChange) => {
+    return thingToChange.charAt(0).toUpperCase() + thingToChange.slice(1)
+  }
+});
