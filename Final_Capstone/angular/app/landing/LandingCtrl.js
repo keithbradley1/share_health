41c7655e6c9687911f7382f63be68459ba954ab2
Keith <keithbradley1@msn.com>
2016-09-16 11:15:42 -0500

    create the rootAPI call in LandingCtrl.js

---
+++ Final_Capstone/angular/app/landing/LandingCtrl.js
@@ -0,0 +1,22 @@
+angular.module("Share_Health")
+  .controller('LandingCtrl',
+  [
+    '$scope',
+    '$http',
+    'RootFactory',
+    '$timeout',
+    function($scope, $http, RootFactory, $timeout){
+      $scope.title = "I'm the landing page";
+      $scope.apiRoot = null;
+
+      RootFactory.getApiRoot()
+        .then(
+          res => {
+            console.log("res: ", res);
+            $scope.apiRoot = res;
+            $timeout();
+          },
+          err => console.log('error', err)
+        );
+    }
+  ]);
