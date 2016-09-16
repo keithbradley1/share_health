2c88ee567600797c31ee1494fdffae352fd155c4
Keith <keithbradley1@msn.com>
2016-09-16 11:22:10 -0500

    create the route to landing.html

---
+++ Final_Capstone/angular/app/landing/landingConfig.js
@@ -0,0 +1,8 @@
+// angular.module('Share_Health')
+//   .config(($routeProvider) => {
+//     $routeProvider
+//       .when("/", {
+//         controller: "LandingCtrl",
+//         templateUrl: "/app/landing/landing.html"
+//       })
+//   })
