ec7a844c7c81658ffc10f68d75ef3816732ea252
Keith <keithbradley1@msn.com>
2016-09-20 10:36:16 -0500

    add partial files and new css file

--- Final_Capstone/angular/app/app.js
+++ Final_Capstone/angular/app/app.js
@@ -2,10 +2,15 @@ angular.module('Share_Health', ['ngRoute'])
   .factory('RootFactory', ($http, $timeout) => {
     let apiRoot = null;
     let httpGet = $http.get('http://localhost:8000');
+    let foodApiRoot = null;
+    let httpGet = $http.get('https://api.nutritionix.com/v1_1/search/fields="item_name","brand_name","nf_calories","nf_total_fat","nf_protein","nf_serving_weight_grams", "images_front_full_url"&appId=41da17b3&appKey=6d6be88bad74c633e3d0b3a183f9d9eb');
 
     return {
       getApiRoot: () => {
           return httpGet.then(res => res.data)
+      }
+      getFoodApiRoot: () => {
+          return httpGet.then(food => food.data)
       }
     }
   }
@@ -15,3 +20,17 @@ angular.module('Share_Health', ['ngRoute'])
     return thingToChange.charAt(0).toUpperCase() + thingToChange.slice(1)
   }
 });
+
+{
+  "appId":"41da17b3",
+  "appKey":"6d6be88bad74c633e3d0b3a183f9d9eb",
+  "query":"",
+  "fields":["item_name","brand_name","nf_calories","nf_total_fat","nf_protein","nf_serving_weight_grams", "images_front_full_url"],
+  "sort":{
+    "field":"_score",
+    "order":"desc"
+  },
+  "filters":{
+    "item_type":2
+  }
+}
